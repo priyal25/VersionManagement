67b3c914f0a70e992378474ed6418bb879929427
Priyal Jain <priyal.j25@gmail.com>
2020-01-15 15:34:38 +0530

    Project Setup

---
+++ VersionCheck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VersionCheck
+//
+//  Created by Priyal on 15/01/20.
+//  Copyright © 2020 Priyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
